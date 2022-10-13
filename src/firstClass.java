d792abb9fccb7cbfe9dd3f34f4ffd7d9db5307ad
Rebeka <rebeka@Rebekas-MacBook-Pro.local>
2022-10-12 20:20:48 -0400

    first commit

---
+++ src/firstClass.java
@@ -0,0 +1,42 @@
+public class firstClass {
+
+    public static void main(String[] args) {
+        System.out.println("My name");
+
+
+
+        // while loop
+        int i = 0;
+        while (i <= 50) {
+            System.out.println(i);
+            i++;
+        }
+
+        //primitive
+        //Way to a set variable=> datatype name =value
+        int a = 6;
+        System.out.println(a);
+        long b = 123456789;
+        System.out.println(b);
+        double c = 3.12346;
+        System.out.println(c);
+        char d = 'k';
+        System.out.println(d);
+
+        int z = 4;
+        int y = 5;
+        z++; // decrement by 1
+        z+=4;
+        System.out.println(z+y);
+        System.out.println(y-z);
+        System.out.println(z==y);
+        System.out.println(z==z);
+        System.out.println(z-y);
+        System.out.println(z >= y);
+        System.out.println(z <= y);
+
+
+
+
+    }
+}
